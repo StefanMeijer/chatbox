a476c68a58ea9431a39d2607324d6d633cbcba68
Stefan <shh.meijer@student.alfa-college.nl>
2020-11-16 23:14:18 +0100

    Admin remove chats
    admin can now remove chat + added styling for chaning password of users

--- page/admin.php
+++ page/admin.php
@@ -4,5 +4,49 @@ if (!isAdmin()) {
     header('location: index.php?page=login');
 }
 
+//  Load in chat messages
+try {
+    $select = $db->prepare("SELECT * FROM users INNER JOIN chat ON chat.users_ID = users.users_ID ORDER BY chat_ID DESC");
+    $select->setFetchMode(PDO::FETCH_ASSOC);
+    $select->execute();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+//  Load in users
+try {
+    $select2 = $db->prepare("SELECT * FROM users WHERE users.user_type = 'user'");
+    $select2->setFetchMode(PDO::FETCH_ASSOC);
+    $select2->execute();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+if (isset($_POST['delete']) && isAdmin()) {
+    deleteMessage($db);
+}
+if (isset($_POST['userDetails']) && isAdmin()) {
+    userInfo($db);
+}
+
+function userInfo($db) {
+    echo 'hpi';
+}
+
+//  Function to delete chat-messages
+function deleteMessage($db)
+{
+    //  Gettings the ID value of chat that has to be deleted
+    $id = htmlspecialchars($_POST['chatID']);
+
+    //Query for deleting data
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "DELETE FROM chat WHERE chat_ID='$id' ";
+    $sth  = $db->prepare($query);
+    $sth->execute();
+    
+    //If succesfull redirect back to page
+    echo "<script>location.href='index.php?page=admin';</script>";
+}
+
 include_once('views/adminPage.php');
-?>
--- views/adminPage.php
+++ views/adminPage.php
@@ -21,3 +21,83 @@ if (!isset($_GET["page"])) {
         </div>
     </div>
 </div>
+
+<div class="container-fluid" id="adminContainer">
+
+    <div>
+        <h5>Verander een gebruiker zijn wachtwoord</h5>
+        <form method="POST">
+            <div class="form-group">
+                <select name="users" id="users">
+                    <?php
+                    while ($data=$select2->fetch()) {
+                        ?>
+                    <option
+                        value="<?php echo $data['username']; ?>">
+                        <?php echo $data['username']; ?>
+                    </option>
+                    <?php
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <table class="table" style="width:30%">
+                    <tr style="border: 2px solid black">
+                        <th>ID</th>
+                        <th>Gebruikersnaam</th>
+                        <th>Email</th>
+                        <th>Verander wachtwoord</th>
+                    </tr>
+                    <tr style="border: 2px solid black">
+                        <td>1</td>
+                        <td>stefan</td>
+                        <td>stefan@stefan.nl</td>
+                        <td>Wachtwoord veranderen</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-border" name="userDetails">Krijg<br>Gebruikers info</button>
+            </div>
+        </form>
+    </div>
+
+    <div>
+        <h5>Beheer verstuurde berichten</h5>
+        <div style="height: 700px;position:relative;">
+            <div style="max-height:100%;overflow:auto;">
+                <table class="table">
+                    <tr style="border: 2px solid black">
+                        <th>Gebruikersnaam</th>
+                        <th>Bericht</th>
+                        <th>Bericht verwijderen</th>
+                    </tr>
+                    <?php
+            while ($data=$select->fetch()) {
+                ?>
+                    <tr style="border: 2px solid black">
+                        <td>
+                            <?php echo $data['username']; ?>
+                        </td>
+                        <td>
+                            <?php echo $data['text']; ?>
+                        </td>
+                        <td>
+                            <form method="POST">
+                                <input type="hidden" name="chatID"
+                                    value="<?php echo $data['chat_ID']; ?>">
+                                <input type="submit" class="btn" name="delete" value="Verwijderen">
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+            }
+            ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
